a8fd83e90d816b79052d7f155d9f54f2de62cefa
Admin <Admin@DESKTOP-QKDEELR>
2024-03-18 21:47:01 +0200

    Add project files.

---
+++ lr7/Controllers/FileController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lr7.Controllers
+{
+    public class FileController : Controller
+    {
+        public IActionResult DownloadFile()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> DownloadFile(string firstName, string lastName, string fileName)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View();
+            }
+
+            string content = $"First Name: {firstName}\nLast Name: {lastName}";
+
+            byte[] fileBytes = Encoding.UTF8.GetBytes(content);
+
+            return File(fileBytes, "text/plain", fileName + ".txt");
+        }
+    }
+}
